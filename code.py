0960ea00f766baca4801b45005060b09eca08848
Shade Alabsa <shade34321@gmail.com>
2021-10-01 23:01:12 -0400

    Lets write the codes out

--- code.py
+++ code.py
@@ -2,23 +2,27 @@ import pulseio
 import board
 import adafruit_irremote
 
-# Create a 'pulseio' input, to listen to infrared signals on the IR receiver
 pulsein = pulseio.PulseIn(board.IR_RX, maxlen=120, idle_state=True)
-# Create a decoder that will take pulses and turn them into numbers
 decoder = adafruit_irremote.GenericDecode()
 
-while True:
-    pulses = decoder.read_pulses(pulsein)
-    try:
-        # Attempt to convert received pulses into numbers
-        received_code = decoder.decode_bits(pulses)
-    except adafruit_irremote.IRNECRepeatException:
-        # We got an unusual short code, probably a 'repeat' signal
-        # print("NEC repeat!")
-        continue
-    except adafruit_irremote.IRDecodeException as e:
-        # Something got distorted or maybe its not an NEC-type remote?
-        print("Failed to decode: ", e.args)
-        continue
+# Power button turned on
+#>>> pulses
+#[9014, 4416, 619, 510, 619, 508, 590, 1642, 616, 511, 598, 504, 625, 502, 585, 544, 595, 505, 624, 1633, 594, 1638, 590, 538, 590, 1640, 587, 1644, 624, 1633, 595, 1635, 611, 1619, 620, 508, 589, 539, 590, 511, 618, 511, 618, 511, 597, 504, 615, 1643, 595, 506, 623, 1634, 592, 1639, 588, 1642, 616, 1641, 597, 1633, 583, 1648, 621, 508, 590, 1640, 618]
+#>>>
 
-    print("NEC Infrared code received: ", received_code)
+# What we should get back from decode but aren't
+# IRMessage(pulses=(9014, 4416, 619, 510, 619, 508, 590, 1642, 616, 511, 598, 504, 625, 502, 585, 544, 595, 505, 624, 1633, 594, 1638, 590, 538, 590, 1640, 587, 1644, 624, 1633, 595, 1635, 611, 1619, 620, 508, 589, 539, 590, 511, 618, 511, 618, 511, 597, 504, 615, 1643, 595, 506, 623, 1634, 592, 1639, 588, 1642, 616, 1641, 597, 1633, 583, 1648, 621, 508, 590, 1640, 618), code=(223, 32, 253, 2))
+
+while True:
+    with open("codes.txt", "a") as output:
+        pulses = decoder.read_pulses(pulsein)
+        try:
+            received_code = decoder.decode_bits(pulses)
+            output.write(pulses)
+        except adafruit_irremote.IRNECRepeatException:
+            continue
+        except adafruit_irremote.IRDecodeException as e:
+            print("Failed to decode: ", e.args)
+            continue
+
+        output.write("NEC Infrared code received: ", received_code)
